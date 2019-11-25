075124a766149d1a8894d0635212e1584eae4210
Cristian Cristea <cristiancristea.1987@gmail.com>
2019-11-25 14:06:53 +0100

    add bookmarked to users

---
+++ db/migrate/20191125130455_add_bookmarked_to_user_articles.rb
@@ -0,0 +1,5 @@
+class AddBookmarkedToUserArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :user_articles, :bookmarked, :boolean, default: :false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_25_112838) do
+ActiveRecord::Schema.define(version: 2019_11_25_130455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2019_11_25_112838) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "bookmarked", default: true
     t.index ["article_id"], name: "index_user_articles_on_article_id"
     t.index ["user_id"], name: "index_user_articles_on_user_id"
   end
