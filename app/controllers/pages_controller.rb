adc9a6fbed7a80e21cff1640762ce6f2b4f6f24e
Cristian Cristea <cristiancristea.1987@gmail.com>
2019-11-26 16:16:30 +0100

    add dashboard cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,12 @@ class PagesController < ApplicationController
   end
 
   def dashboard
+    @read_articles = UserArticle.where(user: current_user, read: true)
+    if @read_articles.nil?
+      @current_article = current_user.user_articles.last.article
+    else
+      @current_article = current_user.profession.topics.first.articles.first
+    end
+    @current_topic = @current_article.topic
   end
 end
