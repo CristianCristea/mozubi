10766db1976702819256adf32fef0ba4c97fe130
Cristian Cristea <cristiancristea.1987@gmail.com>
2019-11-27 11:11:57 +0100

    add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  # a new user can't sign up because it has to belong to a profession
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
