cea8db9cc4f3c89ebd00c139a2f409a8811f0ed5
Cristian Cristea <cristiancristea.1987@gmail.com>
2019-11-28 11:56:24 +0100

    fix dashboard show next article

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -2,11 +2,17 @@ class FlashcardsController < ApplicationController
   def show
     @article = Article.find(params[:article_id])
     @flashcard = Flashcard.find(params[:id])
+    user_article = UserArticle.find_by(article: @article)
 
-    UserArticle.create(
-      user: current_user,
-      article: @article,
-      read: true
-    )
+    if user_article.nil?
+      UserArticle.create(
+        user: current_user,
+        article: @article,
+        read: true
+      )
+    else
+      user_article.update(read: true)
+    end
+
   end
 end
